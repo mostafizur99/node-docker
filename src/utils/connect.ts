192a7f7d0a0064784dd3e7e81815f08a28bcb093
mostafizur99 <coder.mr11@gmail.com>
2022-10-04 08:21:33 +0600

    perf: connection-Db updated

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -5,7 +5,11 @@ const connectDB = async () => {
     const database_uri = process.env.MONGODB_URI as string
 
     try {
-        await mongoose.connect(database_uri)
+        await mongoose.connect(database_uri, {
+            dbName: process.env.MONGODB_DATABASE,
+            user: process.env.MONGODB_USER,
+            pass: process.env.MONGODB_PASSWORD,
+        })
         logger.info("Database Connected")
     } catch (e) {
         logger.error("Could not connect to database")
